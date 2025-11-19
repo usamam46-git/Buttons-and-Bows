a3825c20aff40a2b4762ed634ae1a80b9550d0fb
usamam46-git <usamam46@gmail.com>
2025-11-20 01:39:24 +0500

    Completed programs component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 'use client'
 
 import Hero from '@/components/layout/hero'
+import Programs from '@/components/layout/programs'
 import React from 'react'
 
 const home = () => {
   return (
     <>
      <Hero /> 
+     <Programs />
     </>
   )
 }
